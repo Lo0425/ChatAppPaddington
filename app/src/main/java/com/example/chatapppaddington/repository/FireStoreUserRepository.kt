29f8f8c70c312fe840ad0860a2ce603e8615847d
Lo0425 <chingclo.lcc@gmail.com>
2023-03-16 11:04:12 +0800

    first commit

---
+++ app/src/main/java/com/example/chatapppaddington/repository/FireStoreUserRepository.kt
@@ -0,0 +1,5 @@
+package com.example.chatapppaddington.repository
+
+class FireStoreUserRepository(private val ref: CollectionReference) : UserRepository {
+
+}
---
+++ app/src/main/java/com/example/chatapppaddington/repository/UserRepository.kt
@@ -0,0 +1,5 @@
+package com.example.chatapppaddington.repository
+
+interface UserRepository {
+
+}
